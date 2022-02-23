fe2b53c9cc6f26c8ae2db32f0365187229e00e84
kpavlunenko <kpavlunenko@gmail.com>
2022-02-23 11:12:58 +0200

    task

---
+++ src/main/java/com/example/task_analytical_tool/TaskAnalyticalToolApplication.java
@@ -0,0 +1,14 @@
+package com.example.task_analytical_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAnalyticalToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskAnalyticalToolApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/example/task_analytical_tool/TaskAnalyticalToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_analytical_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskAnalyticalToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
